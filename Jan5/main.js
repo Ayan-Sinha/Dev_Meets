85ea03813bd480f4036feee82a035b1c12bd0ab3
XXXMARTAND <kishor.spd@gmail.com>
2020-01-05 05:03:21 +0530

    first meet

---
+++ Jan5/main.js
@@ -0,0 +1,14 @@
+
+//     const apikey = '$2a$10$qmus06s8/xrTZNC9dikH4.UH5TB6S.RZ.//IrgjwxRJVyq6fLIdGG';
+// const url = new URL('https://www.potterapi.com/v1/characters/'), params = {key: apikey, house: 'Slytherin'}
+
+// Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.onreadystatechange = function() {
+//       if (this.readyState == 4 && this.status == 200) {
+//        document.getElementById("demo").innerHTML = this.responseText;
+//        console.log(this.responseText);
+//       }
+//     };
+//     xhttp.open("GET",url, true);
+//     xhttp.send();
